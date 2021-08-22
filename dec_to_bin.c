acff2a0630bef95ece566056c68f200c5566331d
udt12345 <86314940+udt12345@users.noreply.github.com>
2021-08-22 11:52:32 +0530

    Update dec_to_bin.c

--- dec_to_bin.c
+++ dec_to_bin.c
@@ -1,19 +1,4 @@
-// #include<stdio.h>
-// void main()
-// {
-//     int no,r,i,sum=0;
-//     printf("Enter the number:-");
-//     scanf("%d",&no);   
-//     while(no>0)
-//     {
-//         r=no%2;
-//         no=no/2;
-//         
-//         printf("%d",r);
-//     }
-//    //printf("%d",sum);
-//    // printf("%d",r);
-// }
+
 
 #include<stdio.h>
 int bin(int );
